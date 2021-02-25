216876442ec09e305a04275ba345281abb33e302
khalish29 <68719228+khalish29@users.noreply.github.com>
2021-02-25 16:24:27 +0700

    Add files via upload

---
+++ LoadImage JSON/LoadImage JSON/View/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  LoadImage JSON
+//
+//  Created by macOS User on 08/02/21.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            
+            Home()
+                .navigationBarTitle("News")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Home: View {
+    
+    @ObservedObject var Books = NewsModel()
+    @State var show = false
+    @State var url = ""
+    
+    var body: some View  {
+        List(Books.data) { i in
+            HStack{
+                if i.image != ""{
+                    
+                    WebImage(url: URL(string: i.image)!)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 120, height: 170)
+                        .background(Image("loader")
+                                        .resizable()
+                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                        .frame(width: 60, height: 30))
+                        .cornerRadius(10)
+                } else {
+                    Image("loader")
+                        .resizable()
+                        .frame(width: 120, height: 170)
+                }
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    Text(i.title).fontWeight(.bold)
+                    
+                    Text(i.description).font(.caption).lineLimit(4).multilineTextAlignment(.leading)
+                }
+            }
+            
+        }
+    }
+    
+}
